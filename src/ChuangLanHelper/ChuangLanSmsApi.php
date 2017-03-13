fe0ee4be47835a2665a38a760d018c47bad7fbe7
yinzhen <arthur.yinzhen@outlook.com>
2017-03-13 16:59:18 +0800

    create chuanglanSMS

---
+++ src/ChuangLanHelper/ChuangLanSmsApi.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Arthur\Chuanglan\ChuangLanHelper;
+
+/**
+ * 创蓝短信接口
+ * Class ChuangLanSmsApi
+ * @package Arthur\Chuanglan\ChuangLanHelper
+ */
+class ChuangLanSmsApi
+{
+    private $apiSendUrl = 'http://sms.253.com/msg/HttpBatchSendSM';//创蓝发送短信接口URL
+    private $apiBalanceQueryUrl = 'http://sms.253.com/msg/QueryBalance'; //创蓝短信余额查询接口URL
+    private $apiAccount = ''; //创蓝账号
+    private $apiPassword = ''; //创蓝密码
+
+    /**
+     * ChuangLanSmsApi constructor.
+     * @param $apiAccount
+     * @param $apiPassword
+     */
+    public function __construct($apiAccount, $apiPassword)
+    {
+        $this->apiAccount = $apiAccount;
+        $this->apiPassword = $apiPassword;
+    }
+
+    /**
+     * 设置帐号密码
+     * @param $apiAccount
+     * @param $apiPassword
+     */
+    public function setConfig($apiAccount, $apiPassword)
+    {
+        $this->apiAccount = $apiAccount;
+        $this->apiPassword = $apiPassword;
+    }
+
+    /**
+     * 发送短信
+     * @param $mobile   手机号码
+     * @param $msg  短信内容
+     * @param string $needstatus 是否需要状态报告
+     * @return mixed
+     */
+    public function sendSMS($mobile, $msg, $needstatus = 'false')
+    {
+        //创蓝接口参数
+        $postArr = array(
+            'account' => $this->apiAccount,
+            'pswd' => $this->apiPassword,
+            'msg' => $msg,
+            'mobile' => $mobile,
+            'needstatus' => $needstatus
+        );
+        $result = $this->curlPost($this->apiSendUrl, $postArr);
+        return $result;
+    }
+
+    /**
+     * 查询额度, 地址
+     * @return mixed
+     */
+    public function queryBalance()
+    {
+        //查询参数
+        $postArr = array(
+            'account' => $this->apiAccount,
+            'pswd' => $this->apiPassword,
+        );
+        $result = $this->curlPost($this->apiBalanceQueryUrl, $postArr);
+        return $result;
+    }
+
+    /**
+     * 处理返回值
+     * @param $result
+     * @return array
+     */
+    public function execResult($result)
+    {
+        $result = preg_split("/[,\r\n]/", $result);
+        return $result;
+    }
+
+    /**
+     * 通过CURL发送HTTP请求
+     * @param string $url //请求URL
+     * @param array $postFields //请求参数
+     * @return mixed
+     */
+    private function curlPost($url, $postFields)
+    {
+        $postFields = http_build_query($postFields);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+    }
+
+    //魔术获取
+    public function __get($name)
+    {
+        return $this->$name;
+    }
+
+    //魔术设置
+    public function __set($name, $value)
+    {
+        $this->$name = $value;
+    }
+}
---
+++ src/ChuangLanSMS.php
@@ -0,0 +1,119 @@
+<?php
+namespace Arthur\Chuanglan;
+use Arthur\Chuanglan\ChuangLanHelper\ChuangLanSmsApi;
+use Arthur\Chuanglan\ChuangLanHelper\ChuangLanVoiceApi;
+
+Class ChuangLanSMS {
+    private $appName = null;//签名
+    private $apiAccount = ''; //创蓝账号
+    private $apiPassword = ''; //创蓝密码
+    /**
+     * 初始化
+     * @param type $appName
+     * @param type $apiAccount
+     * @param type $apiPassword
+     */
+    public function __construct($appName,$apiAccount,$apiPassword) {
+        $this->appName = $appName;
+        $this->apiAccount = $apiAccount;
+        $this->apiPassword = $apiPassword;
+    }
+    /**
+     * 设置签名
+     * @param type $appName
+     */
+    public function setAppName($appName) {
+        $this->appName = $appName;
+    }
+    /**
+     * 设置帐号密码
+     * @param $apiAccount
+     * @param $apiPassword
+     */
+    public function setConfig($apiAccount,$apiPassword){
+        $this->apiAccount = $apiAccount;
+        $this->apiPassword = $apiPassword;
+    }
+    /**
+     * 发送文本短信消息
+     * @param $phone 电话号码
+     * @param $code 短信验证码
+     * @return bool
+     */
+    public function sendSms($phone, $code) {
+        $clapi = new ChuanglanSmsApi($this->apiAccount,$this->apiPassword);
+        $result = $clapi->sendSMS("{$phone}", "【{$this->appName}】您好，您的验证码是{$code}", "false");
+        $result = $clapi->execResult($result);
+        if (isset($result[1]) && $result[1] == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /**
+     * 发送语音短信消息
+     * @param type $phone 电话号码
+     * @param type $code 短信验证码
+     * @return bool
+     */
+    public function sendVoice($phone, $code) {
+        $clapi = new ChuanglanVoiceApi($this->apiAccount,$this->apiPassword);
+        $result = $clapi->sendSMS("{$phone}", "【{$this->appName}】您好，您的验证码是{$code}", "false");
+        $result = $clapi->execResult($result);
+        if (isset($result[1]) && $result[1] == 0) {
+            return true;
+        } else {
+            return faslse;
+        }
+    }
+    /**
+     * 文本短信消息查询
+     */
+    public function querySms() {
+        $clapi = new ChuanglanSmsApi();
+        $result = $clapi->execResult($clapi->queryBalance());
+        if (isset($result[1]) && $result[1]) {
+            switch ($result[1]) {
+                case 0:
+                    echo "剩余{$result[3]}条";
+                    break;
+                case 101:
+                    echo '无此用户';
+                    break;
+                case 102:
+                    echo '密码错';
+                    break;
+                case 103:
+                    echo '查询过快';
+                    break;
+            }
+        } else {
+            echo "查询失败";
+        }
+    }
+    /**
+     * 语音短信消息查询
+     */
+    public function queryVoice() {
+        $clapi = new ChuanglanVoiceApi();
+        $result = $clapi->execResult($clapi->queryBalance());
+        if (isset($result[1]) && $result[1]) {
+            switch ($result[1]) {
+                case 0:
+                    echo "剩余{$result[3]}条";
+                    break;
+                case 101:
+                    echo '无此用户';
+                    break;
+                case 102:
+                    echo '密码错';
+                    break;
+                case 103:
+                    echo '查询过快';
+                    break;
+            }
+        } else {
+            echo "查询失败";
+        }
+    }
+}
